aa399ca7e17b6bc8f2dfcce2560113832dc23f68
Valery <valerypopov1995@gmail.com>
2024-12-13 14:01:36 +0300

    init ValPocket

---
+++ Assets/UNSORTED/DebugTools.cs
@@ -0,0 +1,52 @@
+﻿using IngameDebugConsole;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
+
+namespace CraneRcs
+{
+    public class DebugTools : MonoBehaviour
+    {
+        [SerializeField] private GameObject debugTools;
+        private static bool inited;
+
+        private void Awake()
+        {
+            if (inited)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            DontDestroyOnLoad(gameObject);
+            inited = true;
+
+            DebugLogConsole.AddCommand("System_UnloadUnusedAssets", "", () => Resources.UnloadUnusedAssets());
+            DebugLogConsole.AddCommand("System_ClearCache", "", () => Caching.ClearCache());
+            DebugLogConsole.AddCommand("System_GarbageCollect", "", () => System.GC.Collect());
+
+            DebugLogConsole.AddCommand<int>("System_LoadScene", "", index => SceneManager.LoadSceneAsync(index));
+
+            debugTools.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F1))
+                debugTools.SetActive(!debugTools.activeSelf);
+        }
+
+        private bool Find<T>(out T component) where T : MonoBehaviour
+        {
+            component = FindFirstObjectByType<T>();
+
+            if (component == null)
+            {
+                Debug.LogWarning("No component to use in Debug Tools");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Assets/UNSORTED/ThreadContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Gimbal.UI
+{
+    /// <summary>
+    /// Thread context to execute anything in setted (main) thread
+    /// </summary>
+    public static class ThreadContext
+    {
+        private static SynchronizationContext _context;
+
+        public static void Set()
+        {
+            _context = SynchronizationContext.Current;
+        }
+
+        public static void Execute(Action action)
+        {
+            _context.Post((obj) => action(), null);
+        }
+    }
+}
